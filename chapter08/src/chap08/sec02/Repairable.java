acba768902d91afc56d77d97b008d36cf2fc1b5b
solsol7 <som7499@naver.com>
2023-05-25 20:00:38 +0900

    chap08.sec02질문

--- chapter08/src/chap08/sec02/Repairable.java
+++ chapter08/src/chap08/sec02/Repairable.java
@@ -44,9 +44,9 @@ class SCV extends GroundUnit implements Repairable{
 		return "SCV";
 	}
 	
-	public void repair(Repairable r) {
-		if(r instanceof Unit) {
-			Unit u=(Unit)r;
+	public void repair(Repairable r) {		//Repairable이 인터페이스고
+		if(r instanceof Unit) {				//r에 자식클래스가 오고, Unit이 부모클래스인데
+			Unit u=(Unit)r;					//부모클래스 타입의 변수에 자식클래스타입을 넣는데 왜 강제형변환해야함?
 			while(u.hitpoint != u.MAX_HP) {
 				u.hitpoint++;
 			}
