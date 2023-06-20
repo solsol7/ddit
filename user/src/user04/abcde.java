87c75bc4df33866528989b3dcff4f41a5b91f6d7
solsol7 <som7499@naver.com>
2023-06-21 00:05:50 +0900

    숙제

--- user/src/user04/abcde.java
+++ user/src/user04/abcde.java
@@ -1,17 +1,27 @@
 package user04;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class abcde {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		String a=sc.next();
+		String[] listName= {"가나","다라","마바","사아","자차"};
 		
-		for(int i=0; i<a.length(); i++) {
-			System.out.println(a.charAt(i));
+		List<String> list=new ArrayList<>();
+		for(String str : listName) {
+			list.add(str);
 		}
-
+		
+		Map<Integer, String> map=new HashMap<>();
+		for(int i=0; i<listName.length; i++) {
+			map.put(i, listName[i]);
+		}
+		
+		System.out.println(map.get(3));
 	}
 
 }
